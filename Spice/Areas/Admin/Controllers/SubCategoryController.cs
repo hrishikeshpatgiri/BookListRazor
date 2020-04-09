eed234c3e706a3ff45ed919493bcade4ceb9c00d
hrishikeshpatgiri <54248582+hrishikeshpatgiri@users.noreply.github.com>
2020-04-09 18:28:15 +0530

    9th April

---
+++ Spice/Areas/Admin/Controllers/SubCategoryController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+using Spice.Models.VIewModels;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SubCategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public SubCategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var subCatagories = await _db.SubCategory.Include(s=>s.category).ToListAsync();
+            return View(subCatagories);
+        }
+
+        //GET-Create
+        public async Task<IActionResult> Create()
+        {
+            SubCategoryAndCategoryViewModel model = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = await _db.Category.ToListAsync(),
+                SubCategory = new Models.SubCategory(),
+                SubCategoryList = await _db.SubCategory.OrderBy(p => p.Name).Select(p => p.Name).Distinct().ToListAsync()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(SubCategoryAndCategoryViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var doesSubcategoryExists = _db.SubCategory.Include(s => s.category).Where(s => s.Name == model.SubCategory.Name && s.category.Id == model.SubCategory.CategoryId);
+                if(doesSubcategoryExists.Count()>0)
+                {
+
+                }
+                else
+                {
+                    _db.SubCategory.Add(model.SubCategory);
+                    await _db.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            SubCategoryAndCategoryViewModel modelVM = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = await _db.Category.ToListAsync(),
+                SubCategory = model.SubCategory,
+                SubCategoryList = await _db.SubCategory.OrderBy(p => p.Name).Select(p => p.Name).ToListAsync()
+            };
+
+            return View(modelVM);
+        }
+
+    }
+}
---
+++ Spice/Models/SubCategory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Models
+{
+    public class SubCategory
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Sub Category Name")]
+        public string Name { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        public virtual Category category { get; set; }
+    }
+}
